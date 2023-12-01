5999d34863893b70a5e25c37ec4dac5e2c4b0263
MrMond <monsti2410@gmail.com>
2023-12-01 19:06:04 +0100

    working display

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -1,19 +1,29 @@
-<script>// setup>
+<script>
 export default {
-  methods:{
-    handleDrop(event){
+  data() {
+    return {
+      image: null,
+    };
+  },
+  methods: {
+    handleDrop(event) {
       event.preventDefault();
 
       const files = event.dataTransfer.files;
-      if(files.length > 0){
+      if (files.length > 0) {
         const image = files[0];
-        this.handleImage(image)
+        this.handleImage(image);
       }
     },
 
-    handleImage(image){
-      //do something with the image
-      console.log("received a picture")
+    handleImage(image) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.image = reader.result;
+      };
+      reader.readAsDataURL(image);
+
+      console.log("received a picture");
     },
   },
 };
@@ -25,15 +35,22 @@ export default {
       @dragover.prevent
       @drop="handleDrop"
   >
-    Drag and Drop Image here
+    <span v-if="!image">Drag and Drop Image here</span>
+    <img v-if="image" :src="image" alt="Uploaded Image" />
   </div>
+  <span v-if="image">Drag and Drop another Image over the one you're working on, in order to replace it</span>
 </template>
 
 <style scoped>
-.image-uploader{
+.image-uploader {
   border: 2px dashed #ffffff;
   padding: 20px;
   text-align: center;
   background-color: rgba(255, 255, 255, 0.15);
+}
+
+.image-uploader img {
+  max-width: 100%;
+  max-height: 2000px; /* You can adjust the max-height as needed */
 }
 </style>
--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -4,8 +4,6 @@ import ImageUploader from "@/components/ImageUploader.vue";
 </script>
 
 <template>
-  <h1>Martin du schaffst das</h1>
-  <div>
-    <image-uploader @display-image="handleDisplayImage"/>
-  </div>
+  <h1>Meow Generator</h1>
+  <image-uploader @display-image="handleDisplayImage"/>
 </template>

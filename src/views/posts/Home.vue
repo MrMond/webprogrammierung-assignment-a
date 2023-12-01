126ac88bf608db6a1e725a955d975fd86b3f11c0
Lukas Runge <Lukas.Runge@fresenius.com>
2023-12-01 13:41:42 +0100

    Ka man geh mir net auf den sack

--- src/views/posts/Home.vue
+++ src/views/posts/Home.vue
@@ -1,6 +1,39 @@
 <script setup>
+import { ref } from 'vue';
 </script>
 
 <template>
-  <h1>Hier kommt die Startseite rein</h1>
+  <div>
+    <h1 class="catchphrase">Let`s get those Cats <br> memed up!</h1>
+
+
+    <router-link to="/CatMemeGenerator">
+      <button class="styled-button">Start generating</button>
+    </router-link>
+  </div>
 </template>
+
+<style scoped>
+/* Style des Button */
+.styled-button {
+  background-color: #63775B;
+  color: #fff;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+}
+</style>
+
+<style scoped>
+.catchphrase {
+  color: white;
+  font-weight: bold;
+  font-family: '72 Black', cursive;
+  font-size: 45px; 
+  margin-bottom: 20px;
+}
+</style>
+
+

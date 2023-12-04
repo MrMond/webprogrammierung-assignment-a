c671c4c306eaa45780a51f70b9abb6e798331880
Lukas Runge <Lukas.Runge@fresenius.com>
2023-12-04 16:08:17 +0100

    gallery changes

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,17 +1,71 @@
-<!-- PostCard.vue -->
-<script setup>
-const props = defineProps(['title', 'imgSrc', 'largeView'])
-</script>
-
 <template>
   <VCard
       class="mx-auto"
       :max-width="largeView ? 600 : 280"
       :height="largeView ? 'auto' : 200"
-      :style="{cursor: largeView ? 'auto' : 'pointer'}"
+      :style="{ cursor: largeView ? 'auto' : 'pointer' }"
   >
     <VCardTitle>{{ title }}</VCardTitle>
     <VDivider />
-    <VImg v-if="imgSrc" :src="imgSrc" :alt="title" :height="largeView ? '100%' : 'auto'" :width="largeView ? '100%' : 'auto'" />
+    <VImg
+        v-if="imgSrc"
+        :src="imgSrc"
+        :alt="title"
+        :height="largeView ? '100%' : 'auto'"
+        :width="largeView ? '100%' : 'auto'"
+    />
+
+    <!-- Likes-Anzeige und Aktualisierung nur in der größeren Ansicht -->
+    <v-row v-if="largeView">
+      <v-col>
+        <span>{{ likes }} Likes</span>
+        <v-btn @click="changeLikesDialog">Ändern</v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- Dialog zur Änderung der Likes -->
+    <v-dialog v-model="changeLikesDialogVisible" max-width="400">
+      <v-card>
+        <v-card-title>Ändere die Likes</v-card-title>
+        <v-card-text>
+          <v-text-field v-model="newLikes" label="Neue Anzahl" type="number" @keydown.enter="saveLikes" />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="saveLikes">Speichern</v-btn>
+          <v-btn @click="cancelLikesChange">Abbrechen</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </VCard>
 </template>
+
+<script>
+export default {
+  props: ['title', 'imgSrc', 'likes', 'largeView'],
+  data() {
+    return {
+      changeLikesDialogVisible: false,
+      newLikes: this.likes,
+    };
+  },
+  methods: {
+    changeLikesDialog() {
+      this.changeLikesDialogVisible = true;
+    },
+    saveLikes() {
+      const newLikesValue = parseInt(this.newLikes);
+      if (!isNaN(newLikesValue)) {
+        this.$emit('update-likes', newLikesValue);
+        this.changeLikesDialogVisible = false;
+      }
+    },
+    cancelLikesChange() {
+      this.changeLikesDialogVisible = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Stile nach Bedarf anpassen */
+</style>
--- src/views/Meowsterpeace.vue
+++ src/views/Meowsterpeace.vue
@@ -1,11 +1,11 @@
-<!-- Gallery.vue -->
 <template>
   <v-container>
     <h1>Meowsterpeace Gallery</h1>
+
     <v-row>
-      <v-col v-for="(card, index) in galleryData" :key="index" cols="auto">
+      <v-col v-for="(card, index) in sortedGalleryData" :key="index" cols="auto">
         <div class="gallery-card-container" @click="showModal(index)">
-          <PostCard :title="card.title" :imgSrc="card.imgSrc"/>
+          <PostCard :title="card.title" :imgSrc="card.imgSrc" :likes="card.likes" @update-likes="updateLikes(index, $event)" />
         </div>
       </v-col>
     </v-row>
@@ -17,7 +17,13 @@
       </template>
       <v-card>
         <v-card-text>
-          <PostCard :title="selectedPostCard.title" :imgSrc="selectedPostCard.imgSrc" :largeView="true"/>
+          <PostCard
+              :title="selectedPostCard.title"
+              :imgSrc="selectedPostCard.imgSrc"
+              :likes="selectedPostCard.likes"
+              :largeView="true"
+              @update-likes="updateLikes(selectedIndex, $event)"
+          />
         </v-card-text>
         <v-card-actions>
           <v-btn @click="closeModal">Close</v-btn>
@@ -29,26 +35,40 @@
 
 <script setup>
 import PostCard from "@/components/PostCard.vue";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const showModalDialog = ref(false);
 const selectedPostCard = ref(null);
+const selectedIndex = ref(null);
 
 const galleryData = ref([
-  { title: "Cat in a Box", imgSrc: "/cat_in_a_box.jpg" },
-  { title: "Fluffy Kitty", imgSrc: "/fluffy_kitty.jpg" },
-  { title: "Cute Kitten", imgSrc: "/cute_kitten.jpg" },
-  { title: "Sleeping Cat", imgSrc: "/sleeping_cat.jpg" },
-  { title: "Adorable Cat", imgSrc: "/adorable_cat.jpg" },
+  { title: "Cat in a Box", imgSrc: "/cat_in_a_box.jpg", likes: 17 },
+  { title: "Fluffy Kitty", imgSrc: "/fluffy_kitty.jpg", likes: 5 },
+  { title: "Cute Kitten", imgSrc: "/cute_kitten.jpg", likes: 0 },
+  { title: "Sleeping Cat", imgSrc: "/sleeping_cat.jpg", likes: 13 },
+  { title: "Adorable Cat", imgSrc: "/adorable_cat.jpg", likes: 11 },
 ]);
 
 const showModal = (index) => {
   selectedPostCard.value = { ...galleryData.value[index] };
   showModalDialog.value = true;
+  selectedIndex.value = index;
 };
 
 const closeModal = () => {
   showModalDialog.value = false;
+};
+
+const sortedGalleryData = computed(() => {
+  return [...galleryData.value].sort((a, b) => b.likes - a.likes);
+});
+
+const updateLikes = (index, newLikes) => {
+  if (index !== null && index >= 0 && index < galleryData.value.length) {
+    galleryData.value[index].likes = newLikes;
+    // Aktualisiere die sortierte Liste, um die Reihenfolge nach Likes zu erhalten
+    sortedGalleryData.value = [...galleryData.value].sort((a, b) => b.likes - a.likes);
+  }
 };
 </script>
 

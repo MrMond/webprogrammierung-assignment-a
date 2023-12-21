a7dceff29a6f449afc142316641c1577ff2be116
MrMond <monsti2410@gmail.com>
2023-12-21 13:25:27 +0100

    backup

--- src/components/localDB.js
+++ src/components/localDB.js
@@ -10,34 +10,35 @@ async function created() {
 
 export async function getDb() {
     return new Promise((resolve, reject) => {
-            let request = window.indexedDB.open("session", 1);
+        let request = window.indexedDB.open("session", 1);
 
-            request.onupgradeneeded = function (event) {
-                console.log("session storage size increased")
-                let db = event.target.result;
-                let objectStore = db.createObjectStore("currentImage", { keyPath: "id", autoIncrement: true });
-            }
+        request.onupgradeneeded = function (event) {
+            console.log("session storage size increased")
+            let db = event.target.result;
+            let objectStore = db.createObjectStore("currentImage", { keyPath: "id", autoIncrement: true });
+            let likeStore = db.createObjectStore("likes", { keyPath: "id", autoIncrement: true });
+        }
 
-            request.onsuccess = function (event) {
-                console.log("new session storage set up");
-                resolve(event.target.result);
-            }
+        request.onsuccess = function (event) {
+            console.log("new session storage set up");
+            resolve(event.target.result);
+        }
 
-            // error handeling
-            request.onerror = function (event) {
-                console.error("DB error while opening: " + event.target.errorCode);
-                reject(event.target.errorCode)
-            }
+        // error handeling
+        request.onerror = function (event) {
+            console.error("DB error while opening: " + event.target.errorCode);
+            reject(event.target.errorCode)
+        }
 
-            request.onblocked = function (event) {
-                console.warn("Database blocked. Please close other tabs with this site open.");
-                reject(event.target.errorCode);
-            }
-        });
+        request.onblocked = function (event) {
+            console.warn("Database blocked. Please close other tabs with this site open.");
+            reject(event.target.errorCode);
+        }
+    });
 }
 
 export async function setDBImage(image) {
-    if(!ready){
+    if (!ready) {
         await created();
     }
     return new Promise((resolve, reject) => {
@@ -57,8 +58,29 @@ export async function setDBImage(image) {
     });
 }
 
+export async function addDBLikes(identifier) {
+    if (!ready) {
+        await created();
+    }
+    return new Promise((resolve, reject) => {
+        var transaction = db.transaction(["likes"], "readwrite");
+        var objectStore = transaction.objectStore("likes");
+
+        var req = objectStore.add({ likedImg: identifier });
+
+        req.onsuccess = function (event) {
+            console.log("set data to session storage");
+            resolve();
+        };
+
+        req.onerror = function (event) {
+            reject(event.target.errorCode);
+        };
+    });
+}
+
 export async function getDBImage() {
-    if(!ready){
+    if (!ready) {
         await created();
     }
     return new Promise((resolve, reject) => {
